294441a397cc73c980667dd302b802f051a3bb1a
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-02-08 23:55:19 -0300

    :sparkles: Adiciona loaders
    - Utiliza o 'next-compose-plugins' para mais do que 1 plugin
    - Utiliza 'next-fonts' para carregar arquivos de fonte
    
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,2 +1,18 @@
 const withImages = require('next-images');
-module.exports = withImages();
+const withFonts = require('next-fonts');
+const withPlugins = require("next-compose-plugins");
+
+module.exports = withPlugins(
+  [
+    withImages,
+    withFonts,
+  ],
+  {
+    distDir: "./.next",
+  },
+);
+
+// module.exports = {
+//     ...withImages(),
+//     ...withFonts(),
+// }
