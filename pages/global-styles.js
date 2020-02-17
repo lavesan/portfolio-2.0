b502fabc9ae10c9bf077172a44213be7b6d4e245
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-02-17 00:11:27 -0300

    :scissors: Remove o botão padrão para fechar os modais do react-modal e adicionar um border-radius
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- pages/global-styles.js
+++ pages/global-styles.js
@@ -2,8 +2,18 @@ import { createGlobalStyle } from 'styled-components';
 import bobbyJonesFont from '../public/static/fonts/Bobby Jones/BobbyJonesSoft.otf';
 
 export default createGlobalStyle`
+    
     @font-face {
         font-family: 'BobbyJonesSoft';
         src: url(${bobbyJonesFont});
     }
+    
+    .styles_modal__gNwvD {
+        border-radius: 5px;
+    }
+
+    .styles_closeButton__20ID4 {
+        display: none;
+    }
+
 `
