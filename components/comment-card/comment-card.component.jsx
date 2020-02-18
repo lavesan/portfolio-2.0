9fca12b82a52d8c8488cdcab2e122b64664879c6
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-02-18 13:35:05 -0300

    :sparkles: Integra os comentários vindo como resposta no redux com uma action
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- components/comment-card/comment-card.component.jsx
+++ components/comment-card/comment-card.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 import { StyledCommentCard } from './comment-card.styles';
+import emptyUser from '../../public/static/imgs/empty-user.png';
 
-export default ({ userImgUrl, name, age, comment }) => {
+export default ({ user, briefComment }) => {
     return (
         <StyledCommentCard>
-            <p className="comment-text">{comment}</p>
+            <p className="comment-text">{briefComment}</p>
             <footer className="comment-card-footer">
                 <div className="comment-card-footer--image-container">
-                    <img src={userImgUrl} alt="ícone do usuário" />
+                    <img src={user.imgUrl ? user.imgUrl : emptyUser} alt="ícone do usuário" />
                 </div>
-                <p>{name}, {age} anos</p>
+                <p>{user.name}, {user.age} anos</p>
             </footer>
         </StyledCommentCard>
     )
