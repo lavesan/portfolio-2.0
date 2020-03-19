fddc8e31b6efbab66108a60b953189273d1507e1
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-03-19 01:20:02 -0300

    :nail_care: Remove a página mockada do início
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,3 @@
-export default function Index() {
-    return (
-        <>
-            <h1>Primeira página</h1>
-            <p>Texto ae</p>
-        </>
-    )
-}
+import InicioView from '../views/inicio';
+
+export default InicioView;
