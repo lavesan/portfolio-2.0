f2846d3c0fdb4aeb96db7be11f3b4b38869b59b5
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-04-03 17:53:01 -0300

    :bug: Resolver bug onde o ícone de procura aparecia gigantesco na tela, ao estar carregando o código
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- components/responsive-search-input/responsive-search-input.component.jsx
+++ components/responsive-search-input/responsive-search-input.component.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import { StyledResponsiveSearchInput, StyledResponsiveSearchComponent } from './responsive-search-input.styles';
 
 export default ({ children, onSubmit, onClick, setFieldValue, ...inputAttrs }) => {
+
+    const [showSearchIcon, setShowSearchIcon] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShowSearchIcon(true);
+        }, 100);
+    }, [])
+
     return (
         <StyledResponsiveSearchComponent>
-            <FontAwesomeIcon className="search-icon" onClick={onSubmit} icon={faSearch} />
+            {showSearchIcon && <FontAwesomeIcon className="search-icon" onClick={onSubmit} icon={faSearch} />}
             <StyledResponsiveSearchInput onClick={onClick} onChange={setFieldValue} {...inputAttrs} />
             {children}
         </StyledResponsiveSearchComponent>
     )
+
 }
