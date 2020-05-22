7ed6d45dd66a776f7afffe12e221251fdc7f41e5
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-05-21 23:37:00 -0300

    :memo: Adds a component with the about section
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

---
+++ views/inicio/about-section/about-section.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { TagcloudComponent } from '../../../components/tagcloud';
+import { StyledAboutSection } from './about-section.styles';
+import { ISection } from '../inicio.interfaces';
+import languages from '../../../public/static/languages.json';
+// @ts-ignore
+import myImage from '../../../public/static/imgs/foto-portfolio.jpeg';
+
+export default ({ triggerAnimation, initialLeftStyle, initialRightStyle, animationStyle, animationTransition }: ISection) => {
+
+    const fasterAnimationTransition = { duration: 0.7 };
+
+    return (
+        <StyledAboutSection>
+            <motion.div
+                initial={initialLeftStyle}
+                animate={triggerAnimation ? animationStyle : initialLeftStyle}
+                transition={fasterAnimationTransition}
+                className="about-section--info-container">
+                <div className="about-section--info-container--img-container">
+                    <img src={myImage} alt="Minha foto" />
+                </div>
+                <div>
+                    <h3 className="about-section--info-container--text-title"><b>Quem é esse cara?</b></h3>
+                    <p>Algo ai</p>
+                </div>
+            </motion.div>
+            <motion.div                    
+                initial={initialLeftStyle}
+                animate={triggerAnimation ? animationStyle : initialLeftStyle}
+                transition={animationTransition}
+                style={{ textAlign: 'center' }}>
+                <TagcloudComponent data={languages} />
+            </motion.div>
+        </StyledAboutSection>
+    )
+
+}
---
+++ views/inicio/about-section/about-section.styles.ts
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components';
+
+export const StyledAboutSection = styled.div`
+    ${({ theme }) => css`
+        .about-section--info-container {
+            display: flex;
+            flex-flow: row nowrap;
+            margin-bottom: 20px;
+
+            .about-section--info-container--img-container {
+                border-radius: 50%;
+                width: 200px;
+                height: 200px;
+                overflow: hidden;
+                margin-right: 30px;
+
+                img {
+                    width: inherit;
+                }
+            }
+
+            .about-section--info-container--text-title {
+                margin: 0;
+                color: ${theme.green.primary};
+            }
+        }
+    `}
+`;
