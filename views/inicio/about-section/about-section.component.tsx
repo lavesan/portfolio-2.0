fc2e2d0a10c29533187e1df0ca77cfca1d6233e4
Valdery Alves Paes Junior <valdery.jur@gmail.com>
2020-05-29 00:52:48 -0300

    :memo: Lets the text better
    Signed-off-by: Valdery Alves Paes Junior <valdery.jur@gmail.com>

--- views/inicio/about-section/about-section.component.tsx
+++ views/inicio/about-section/about-section.component.tsx
@@ -26,8 +26,8 @@ export default ({ triggerAnimation, initialLeftStyle, initialRightStyle, animati
                     <h3 className="about-section--info-container--text-title"><b>Quem sou eu?</b></h3>
                     <p>
                         Me chamo Valdery e tenho 24 anos. Sempre gostei de criar e isso me levou a desenhar e esculpir por hobby. Já na faculdade tive meu primeiro contato com
-                        programação e me enchi de ânimo quando vi que poderia realmente fazer algo que pessoas poderiam
-                        utilizar, sendo isso o que me levou a entrar na área de desenvolvimento.<br />
+                        programação e me enchi de ânimo quando vi que teria a oportunidade de fazer algo que as pessoas utilizariam, entrando
+                        de cabeça na área de desenvolvimento!<br />
                         Com minha experiência eu me sinto bem tranquilo para tirar qualquer sonho do papel e transformá-lo em um software.<br />
                         Bora conversar sobre e planejar para eu partir para ação e te entregar um produto de qualidade?
                     </p>
