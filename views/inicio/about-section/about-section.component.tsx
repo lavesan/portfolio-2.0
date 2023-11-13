cc9250d6e1df618a64663b85bd4c0c289c644e48
Valdery Alves <valdery.jur@gmail.com>
2023-11-13 18:59:30 -0300

    feat: adds marketplace data
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- views/inicio/about-section/about-section.component.tsx
+++ views/inicio/about-section/about-section.component.tsx
@@ -25,11 +25,12 @@ export default ({ triggerAnimation, initialLeftStyle, initialRightStyle, animati
                 <div className="about-section-info">
                     <h3 className="about-section--info-container--text-title"><b>Quem sou eu?</b></h3>
                     <p>
-                        Me chamo Valdery e tenho 24 anos. Sempre gostei de criar e isso me levou a desenhar e esculpir por hobby. Já na faculdade tive meu primeiro contato com
+                        Me chamo Valdery e tenho 28 anos. Sempre gostei de criar e isso me levou a desenhar e esculpir por hobby. Já na faculdade tive meu primeiro contato com
                         programação e me enchi de ânimo quando vi que teria a oportunidade de fazer algo que as pessoas utilizariam, entrando
                         de cabeça na área de desenvolvimento!<br />
                         Com minha experiência eu me sinto bem tranquilo para tirar qualquer sonho do papel e transformá-lo em um software.<br />
-                        Bora conversar sobre e planejar para eu partir para ação e te entregar um produto de qualidade?
+                        Bora conversar sobre e planejar para eu partir para ação e te entregar um produto de qualidade?<br />
+                        A maioria dos projetos que faço são internos e não posso publicar, mas abaixo mostro alguns que já fiz e posso mostrar!
                     </p>
                 </div>
             </motion.div>
